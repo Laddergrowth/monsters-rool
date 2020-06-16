df851724c68dbfe73a750f9b5defaa4f03d93d75
Lucas <45767956+Laddergrowth@users.noreply.github.com>
2020-06-16 18:28:05 +0100

    gh-pages
    Eh nees gh-pages que faz apparecer lah

---
+++ precache-manifest.30747d87edfe9e411ea40ed730451310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d02942a085f4e3a53b3c5e1451c86b9",
+    "url": "/Laddergrowth/monsters-roll/index.html"
+  },
+  {
+    "revision": "800fbd6ba718450208a3",
+    "url": "/Laddergrowth/monsters-roll/static/css/main.f68cbbf8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Laddergrowth/monsters-roll/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Laddergrowth/monsters-roll/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800fbd6ba718450208a3",
+    "url": "/Laddergrowth/monsters-roll/static/js/main.487edf31.chunk.js"
+  },
+  {
+    "revision": "baf04876756eff5e07d7",
+    "url": "/Laddergrowth/monsters-roll/static/js/runtime-main.fb9f15b5.js"
+  }
+]);
